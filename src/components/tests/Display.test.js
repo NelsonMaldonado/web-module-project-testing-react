cb5edf515981e13221ee15b894fced8abdce98ee
Nelson Maldonado <play2nino@gmail.com>
2021-11-09 01:52:52 -0500

    display done

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -5,44 +5,68 @@ import userEvent from "@testing-library/user-event"
 import mockFetchShow from "../../api/fetchShow"
 jest.mock("../../api/fetchShow.js")
 
-test("test if Display component renders without any passed in props", () => {
+const testFullShow = {
+  //add in approprate test data structure here.
+  name: "Test Show",
+  seasons: [
+    {
+      id: 0,
+      name: "Test Season 1",
+      episodes: [],
+    },
+    {
+      id: 1,
+      name: "Test Season 2",
+      episodes: [],
+    },
+    {
+      id: 2,
+      name: "Test Season 3",
+      episodes: [],
+    },
+    {
+      id: 3,
+      name: "Test Season 4",
+      episodes: [],
+    },
+  ],
+  summary: "Test Summary",
+}
+
+test("renders without any errors", () => {
   render(<Display />)
+})
+
+test("When fetch button is pressed, show component will display", async () => {
+  mockFetchShow.mockResolvedValueOnce(testFullShow)
+
+  render(<Display />)
+
+  const button = screen.getByRole("button")
+  userEvent.click(button)
+
+  const showContainer = await screen.findByTestId("show-container")
+
+  expect(showContainer).toBeInTheDocument()
+})
+
+test("when fetch button is pressed, amount of selected options rendered is equal to amount of seasons in test data", async () => {
+  mockFetchShow.mockResolvedValueOnce(testFullShow)
+  const displayFunc = jest.fn()
+
+  render(<Display displayFunc={displayFunc} />)
+
+  const button = screen.getByRole("button")
+  userEvent.click(button)
+
+  await waitFor(() => {
+    expect(displayFunc).toHaveBeenCalled()
+  })
 })
 
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
 //2. Test that the Display component renders without any passed in props.
-const testEpisode = {
-  id: 1,
-  name: "",
-  image:
-    "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
-  season: 1,
-  number: 1,
-  summary: "",
-  runtime: 1,
-}
-
-test("when the fetch button is pressed, the show component will display", async () => {
-  render(<Display />)
-  const button = screen.getByRole("button")
-  userEvent.click(button)
-
-  const findContainer = await screen.findByTestId("show-container")
-  expect(findContainer).toBeInTheDocument()
-})
-
-test("when fetch button is pressed, amount of selected options rendered is equal to amount of seasons is test data", async () => {
-  mockFetchShow.mockResolvedValueOnce(testEpisode)
-  const displayFunction = jest.fn()
-  render(<Display displayFunction={displayFunction} />)
-  const button = screen.getByRole("button")
-  userEvent.click(button)
-
-  await waitFor(() => {
-    expect(displayFunction).toHaveBeenCalled()
-  })
-})
 
 //3. Rebuild or copy a show test data element as used in the previous set of tests.
 //4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
